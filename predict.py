cddde4e2420fb3d1f3027caa7ee39df694045273
linguosaur <36645379+linguosaur@users.noreply.github.com>
2019-01-18 16:19:47 -0500

    Add files via upload
    stores multi-character keys

--- predict.py
+++ predict.py
@@ -6,28 +6,61 @@ def tally(key,dic):
 	else:
 		dic[key] = 1
 
-def updateModel(char,seen,model):
+def getPredictions(predictor,model):
+	if predictor in model:
+		return model[predictor].keys()
+
+	return None
+
+def predictRight(char,predictor,model):
+	predictions = getPredictions(predictor,model)
+	#sys.stdout.write('predictor: ' + predictor + '\n')
+	#sys.stdout.write('prediction: ' + repr(predictions) + '\n')
+	if predictions != None and char in predictions:
+		#sys.stdout.write('prediction correct!\n')
+		return True
+
+	return False
+
+def updateModel(char,seen,predictor,predictionIsRight,model):
 	if seen != '':
-		predictor = seen[-1]
-		sys.stdout.write('predictor: ' + predictor + '\n')
-		if predictor not in model:
-			model[predictor] = {}
-		tally(char,model[predictor])
+		for i in range(len(predictor)):
+			subseq = predictor[i:len(predictor)]
+			if subseq not in model:
+				model[subseq] = {}
+			tally(char,model[subseq])
+
+			if predictionIsRight: # look for previous occurence of subseq in seen
+				newKey = subseq+char
+				if newKey not in model:
+					#sys.stdout.write('adding ' + newKey + ' as key\n')
+					charLastTime = seen[seen.index(newKey)+len(newKey)]
+					model[newKey] = {}
+					tally(charLastTime,model[newKey])
 
 def outputDic(dic):
+	sys.stdout.write('Model:\n')
 	for key in dic:
 		sys.stdout.write(key + ': ' + repr(dic[key]) + '\n')
-	sys.stdout.write('\n')
 
 seen = ''
 model = {}
 inputFileName = sys.argv[1]
 with open(inputFileName) as inputFile:
-	prediction = ''
+	[predictor,prediction] = ['','']
 	char = inputFile.read(1)
 	while char != '':
-		sys.stdout.write('char: ' + char + '\nseen: ' + seen + '\n')
-		updateModel(char,seen,model)
-		outputDic(model)
+		#sys.stdout.write('char: ' + char + '\nseen: ' + seen + '\n')
+		predictionIsRight = predictRight(char,predictor,model)
+		updateModel(char,seen,predictor,predictionIsRight,model)
+		
 		seen += char
+		if predictionIsRight:
+			predictor += char
+		else:
+			predictor = seen[-1]
+		
 		char = inputFile.read(1)
+		#sys.stdout.write('\n')
+	
+	outputDic(model)
