d54cdbbccfa0facaf06087c191ca752e9112cf85
linguosaur <36645379+linguosaur@users.noreply.github.com>
2020-06-26 17:57:32 -0400

    Prunes phrases not in window

--- lz.py
+++ lz.py
@@ -1,52 +1,88 @@
 import sys
 from operator import itemgetter
 
+phrasesInWindowOrdered = []
 phraseBook = {}
+posLastSeen = {}
 longestPrefix = ''
 activePhrases = set([])
+charsRead = 0
 
 BYTES_IN_CHAR = 1
+WINDOW_SIZE = 5000
 
-def updateActivePhrases(string):
+def updateActivePhrases(phrase):
     global phraseBook, activePhrases
     if len(activePhrases) == 0 and len(phraseBook) > 0:
         activePhrases = set(phraseBook.keys())
-    activePhrases = set([s for s in activePhrases if s.startswith(string)])
+    activePhrases = set([p for p in activePhrases if p.startswith(phrase)])
 
-def inPhraseBook(string):
-    updateActivePhrases(string)
+def inPhraseBook(phrase):
+    updateActivePhrases(phrase)
     return len(activePhrases) > 0
+
+def tallyPhraseBook(phrase):
+    global phraseBook
+    if phrase in phraseBook:
+        phraseBook[phrase] += 1
+    else:
+        phraseBook[phrase] = 1        
+
+def inWindow(pos):
+    global charsRead, WINDOW_SIZE
+    if pos >= charsRead - WINDOW_SIZE:
+        return True
+
+    return False
+
+def update():
+    global charsRead, posLastSeen, phrasesInWindowOrdered, phraseBook
+    phrase, pos = phrasesInWindowOrdered[0]
+    while not inWindow(pos):
+        phrasesInWindowOrdered.pop(0)
+        if not inWindow(posLastSeen[phrase]):
+            del phraseBook[phrase]
+            del posLastSeen[phrase]
+        phrase, pos = phrasesInWindowOrdered[0]
 
 def readNextChar(textfile):
     nextChar = textfile.read(BYTES_IN_CHAR)
     if nextChar == None:
         return None
 
-    global phraseBook, longestPrefix
-    if inPhraseBook(longestPrefix+nextChar):
-        phraseBook[longestPrefix+nextChar] += 1
-        longestPrefix += nextChar
+    global charsRead, phrasesInWindowOrdered, phraseBook, posLastSeen, longestPrefix
+    candidate = longestPrefix + nextChar
+    
+    tallyPhraseBook(candidate)
+    startPos = charsRead-len(candidate)+1
+    phrasesInWindowOrdered.append((candidate, startPos))
+    posLastSeen[candidate] = startPos
+    update()
+                           
+    if inPhraseBook(candidate):
+        longestPrefix = candidate
     else:
-        phraseBook[longestPrefix+nextChar] = 1
         longestPrefix = ''
+
+    charsRead += 1
 
     return nextChar
 
 def printPhraseBook():
-    for k,v in sorted(phraseBook.items(), key=itemgetter(1), reverse=True):
-        sys.stdout.write('\t'.join([k,repr(v)]) + '\n')
+    global phraseBook, posLastSeen
+    for phrase,freq in sorted(phraseBook.items(), key=itemgetter(1), reverse=True):
+        sys.stdout.write('\t'.join([phrase,repr(freq),repr(posLastSeen[phrase])]) + '\n')
+
+def processStream():
+    while readNextChar(textFile) != None and charsRead <= 500000:
+        if charsRead % 5000 == 0:
+            sys.stderr.write(repr(charsRead) + ', ')
+            sys.stderr.flush()
+        continue
 
 
 textFilename = sys.argv[1]
 textFile = open(textFilename)
 
-charsRead = 0
-while readNextChar(textFile) != None and charsRead <= 50000:
-    charsRead += 1
-    if charsRead % 5000 == 0:
-        sys.stderr.write(repr(charsRead) + ', ')
-        sys.stderr.flush()
-    
-    continue
-
+processStream()
 printPhraseBook()
