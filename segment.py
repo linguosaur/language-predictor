a60ebb3ddd545a68f3181015b8fab105de76d1d1
linguosaur <36645379+linguosaur@users.noreply.github.com>
2020-07-14 13:41:17 -0400

    Iterates until average code length increases

--- segment.py
+++ segment.py
@@ -3,47 +3,86 @@
 import sys
 from math import log2
 
-freqs = {}
-phraseBook = set([])
 
 def codeLenPerChar(string, text, freq):
-    return log2(len(text)/len(string)/freq)/len(string)
+    stringLen, textLen = len(string), len(text)
+    codeLen = log2(textLen/stringLen/freq)
+    return codeLen/stringLen
+
+def getFreq(string, text, freqsCache, phraseBook, iteration):
+    freq = 0
+    if string in freqsCache:
+        freq = freqsCache[string]
+    else:
+        if iteration == 0:
+            freq = text.count(string)
+        else:
+            possiblePhrases = [phraseBook[p] for p in phraseBook if p.startswith(string)]
+            if len(possiblePhrases) > 0:
+                freq = sum(possiblePhrases)
+            else:
+                freq = 1
+        if freq > 1:
+            freqsCache[string] = freq
+
+    return freq
+
+def tallyPhraseBook(string, phraseBook):
+    if string not in phraseBook:
+        phraseBook[string] = 1
+    else:
+        phraseBook[string] += 1
+
+def cutString(freq, lastCodeLenPerChar, thisCodeLenPerChar):
+    return freq == 1 or lastCodeLenPerChar > 0.0 and thisCodeLenPerChar > lastCodeLenPerChar
 
 def segment(text):
-    string = ''
-    lastCodeLenPerChar = 0.0
-    charsRead = 0
-    for char in text:
-        string += char
-        freq = 0
-        if string in freqs:
-            freq = freqs[string]
-        else:
-            freq = text.count(string)
-            if freq > 1:
-                freqs[string] = freq
+    phraseBook, lastTotalCodeLen = {}, 0.0
+    iteration, iterate = 0, True
+    MAX_ITERATIONS = 5
+    while iterate:
+        string, lastCodeLenPerChar, charsRead, freqsCache, thisTotalCodeLen, newPhraseBook = '', 0.0, 0, {}, 0.0, {}
 
-        thisCodeLenPerChar = codeLenPerChar(string, text, freq)
-        if freq == 1 or lastCodeLenPerChar > 0.0 and thisCodeLenPerChar > lastCodeLenPerChar:
-            phraseBook.add(string[:-1])
-            string = char
-            lastCodeLenPerChar = codeLenPerChar(char, text, freq)
-        else:
-            lastCodeLenPerChar = thisCodeLenPerChar
+        sys.stderr.write('Iteration ' + repr(iteration) + ':\n')
+        for char in text:
+            string += char
+            freq = getFreq(string, text, freqsCache, phraseBook, iteration)
+            thisCodeLenPerChar = codeLenPerChar(string, text, freq)
+
+            if cutString(freq, lastCodeLenPerChar, thisCodeLenPerChar):
+                phrase = string[:-1]
+                tallyPhraseBook(phrase, newPhraseBook)
+                thisTotalCodeLen += len(phrase)*lastCodeLenPerChar
+                string = char
+                lastCodeLenPerChar = codeLenPerChar(char, text, freq)
+            else:
+                lastCodeLenPerChar = thisCodeLenPerChar
             
-        charsRead += 1
-        if charsRead % 1000 == 0:
-            sys.stderr.write(repr(charsRead) + ', ')
-            sys.stderr.flush()
-#        if charsRead == 10000:
-#            return
+            charsRead += 1
+            if charsRead % 10000 == 0:
+                sys.stderr.write(repr(charsRead) + ', ')
+                sys.stderr.flush()
+
+        sys.stderr.write('\Average code length per character: ' + repr(thisTotalCodeLen/len(text)) + '\n')
+        if iteration == 0 or thisTotalCodeLen < lastTotalCodeLen and iteration < MAX_ITERATIONS:
+            iteration += 1
+            lastTotalCodeLen = thisTotalCodeLen
+            phraseBook = newPhraseBook
+        else:
+            iterate = False
+
+        sys.stderr.write('\n')
+
+    return phraseBook
 
 def printPhrases(phraseBook):
-    for phrase in sorted(phraseBook):
-        print(phrase)
+    for phrase, freq in sorted(phraseBook.items()):
+        sys.stdout.write('\t'.join([phrase, repr(freq)]) + '\n')
+
 
 textFilename = sys.argv[1]
-text = open(textFilename).read()
+with open(textFilename) as textFile:
+    text = textFile.read()
 
-segment(text)
+phraseBook = segment(text)
 printPhrases(phraseBook)
